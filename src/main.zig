33c3bfee0e99cb674ca065490b2f986d67d35f42
small dog <grrr@bark.moon>
2025-07-20 18:22:53 +0200

    added 'new' cmd, added 'get-compiler' cmd

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const http = std.http;
+
+const comfyCompilerUrl = "https://github.com/comfy-lang/comfy/releases/download/v0.1.0/comfy";
+const cozyDefaultPath = "~/.cozy/bin";
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
@@ -66,8 +70,64 @@ fn runFile(file: []const u8, writer: anytype) !void {
 }
 
 fn downloadCompiler(path_override: ?[]const u8, writer: anytype) !void {
-    const path = path_override orelse "~/.cozy/bin";
-    // TODO: HTTP download comfy binary to path
-    try writer.print("Downloading compiler to: {s}\n", .{path});
+    const path = path_override orelse cozyDefaultPath;
+    try writer.print("Downloading comfy compiler to: {s}\n", .{path});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+    const heapAlloc = std.heap.page_allocator;
+
+    const home_dir = try std.process.getEnvVarOwned(heapAlloc, "HOME");
+    defer heapAlloc.free(home_dir);
+
+    const full_path = try std.fs.path.join(heapAlloc, &[_][]const u8{
+        home_dir, ".cozy", "bin",
+    });
+    defer heapAlloc.free(full_path);
+
+    var root = try std.fs.openDirAbsolute("/", .{});
+    defer root.close();
+
+    try root.makePath(full_path);
+
+    var client = http.Client{ .allocator = allocator };
+    defer client.deinit();
+
+    const body = try downloadComfyCompilerBinary(&client, allocator);
+    defer allocator.free(body);
+
+    const compiler_path = try std.fs.path.join(heapAlloc, &[_][]const u8{
+        full_path, "comfy",
+    });
+    defer heapAlloc.free(compiler_path);
+
+    const file = try std.fs.createFileAbsolute(compiler_path, .{});
+    defer file.close();
+
+    try file.writeAll(body);
+    try file.chmod(0o755);
+
+    try writer.print("Compiler downloaded successfully to {s}\n", .{compiler_path});
+}
+
+fn downloadComfyCompilerBinary(client: *http.Client, allocator: std.mem.Allocator) ![]const u8 {
+    const uri = try std.Uri.parse(comfyCompilerUrl);
+    const buf = try allocator.alloc(u8, 1024 * 8);
+    defer allocator.free(buf);
+    var req = try client.open(.GET, uri, .{
+        .server_header_buffer = buf,
+    });
+    defer req.deinit();
+
+    try req.send();
+    try req.finish();
+    try req.wait();
+
+    try std.testing.expectEqual(req.response.status, .ok);
+
+    var rdr = req.reader();
+    const body = try rdr.readAllAlloc(allocator, 1024 * 1024 * 4);
+
+    return body;
 }
 
